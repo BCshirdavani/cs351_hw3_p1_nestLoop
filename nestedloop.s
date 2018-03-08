cdb24e93cc1b2787d763ad7b35d9371ff299f875
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-07 20:44:19 -0800

    testing outer row loop...
    error: Illegal instruction

--- nestedloop.s
+++ nestedloop.s
@@ -10,12 +10,46 @@
 
 /* Data Section */
 
+i: .word 0
+j: .word 0
+return: .word 0
+blank: .asciz "_"
+star: .asciz "*"
+formatStr: .asciz "%d"
+
 
 /* Code Section */
 
 .text
 .global main
 main:
-	mov r0, #123
+	ldr r1, addrOfReturn
+	
+	loopRow:
+		ldr r0, addrOfBlank	/* print leading blank	*/
+		bl printf		/* print leading blank	*/
+		
+		
+
+		ldr r3, addrOfI		/* i++	*/
+		ldr r5, [r3]		/* i++	*/
+		add r5, r5, #1		/* i++	*/
+		str r5, [r3]
+		cmp r5, #8		/* (i < 8)	*/
+		blt loopRow	
+	
+
+
 	bx lr
 
+
+
+
+.global printf
+
+addrOfI: .word i
+addrOfJ: .word j
+addrOfReturn: .word return
+addrOfBlank: .word blank
+addrOfStar: .word star
+addrOfFormatStr: .word formatStr
