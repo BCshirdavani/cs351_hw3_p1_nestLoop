895234a8343068cbf7a5edf081bdbdc2200668c6
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-08 22:34:03 -0800

    nest loop fail
    second loop never ends...

--- nestedloop.s
+++ nestedloop.s
@@ -27,19 +27,41 @@ main:
 	str lr, [r1]
 	
 loopRow:
+	ldr r3, addrOfI		/* j = i for inner col loop	*/
+	ldr r4, [r3]		/* j = i for inner col loop	*/
+	ldr r3, addrOfJ		/* j = i for inner col loop	*/
+	str r4, [r3]		/* j = i for inner col loop	*/
+	bl loopLblank		/* send to loop for leading blanks	*/
+
 	ldr r0, addrOfNewline	/* print newline for each row	*/
 	bl printf		/* print newline for each row	*/		
 	ldr r3, addrOfI		/* i++	*/
 	ldr r5, [r3]		/* i++	*/
 	add r5, r5, #1		/* i++	*/
 	str r5, [r3]
+	ldr r2, addrOfJ		/* j = i for inner col loop	*/
+	str r5, [r2]		/* j = i for inner col loop	*/
 	cmp r5, #8		/* (i < 8)	*/
 	blt loopRow	
 
 	ldr r1, addrOfReturn
 	ldr lr, [r1]
 
-bx lr
+
+loopLblank:
+	ldr r0, addrOfBlank	/* load blank char	*/
+	bl printf		/* print blank		*/
+	ldr r3, addrOfJ		/* j++	*/
+	ldr r5, [r3]		/* j++	*/
+	add r5, r5, #1		/* j++	*/
+	str r5, [r3]
+	cmp r5, #8		/* (j < 8)	*/
+	blt loopLblank
+	
+	/* finish the inner pyramid conditionals here */
+	
+end:
+	bx lr
 
 .global printf
 
