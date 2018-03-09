6b5c0bba4b68815ea03587edba21d9c73a08a9f2
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-08 19:45:25 -0800

    single loop working
    TODO: nest other loops

--- nestedloop.s
+++ nestedloop.s
@@ -9,7 +9,7 @@
 /----------------------------------------------*/
 
 /* Data Section */
-
+.data
 i: .word 0
 j: .word 0
 return: .word 0
@@ -24,26 +24,22 @@ formatStr: .asciz "%d"
 .global main
 main:
 	ldr r1, addrOfReturn
+	str lr, [r1]
 	
-	loopRow:
-		ldr r0, addrOfBlank	/* print leading blank	*/
-		bl printf		/* print leading blank	*/
-		
-		
+loopRow:
+	ldr r0, addrOfBlank	/* print leading blank	*/
+	bl printf		/* print leading blank	*/		
+	ldr r3, addrOfI		/* i++	*/
+	ldr r5, [r3]		/* i++	*/
+	add r5, r5, #1		/* i++	*/
+	str r5, [r3]
+	cmp r5, #8		/* (i < 8)	*/
+	blt loopRow	
 
-		ldr r3, addrOfI		/* i++	*/
-		ldr r5, [r3]		/* i++	*/
-		add r5, r5, #1		/* i++	*/
-		str r5, [r3]
-		cmp r5, #8		/* (i < 8)	*/
-		blt loopRow	
-	
+	ldr r1, addrOfReturn
+	ldr lr, [r1]
 
-
-	bx lr
-
-
-
+bx lr
 
 .global printf
 
