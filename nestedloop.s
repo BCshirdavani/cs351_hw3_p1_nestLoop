9da53d930e368efe57080f5261529fb27f702085
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-03-11 20:17:13 -0700

    broken...

--- nestedloop.s
+++ nestedloop.s
@@ -30,8 +30,21 @@ loopRow:
 	ldr r3, addrOfJ		/* j = i for inner col loop	*/
 	str r4, [r3]		/* j = i for inner col loop	*/
 	bl loopLblank		/* send to loop for leading blanks	*/
+
+	@ if (i <= (2*i-1) : go to loop for inner pyramid
+	ldr r7, addrOfI
+	ldr r8, [r7]	
+	@ mul r8, r8, #2	/* 	2*i		*/
+	@ add r8, #0, r8, LSL #1	/* 	2*i		*/
+	mov r8, r8, LSL #1	/* 	2*i		*/
+	sub r8, r8, #1	/* 	2*i-1	*/
+	ldr r9, addrOfJ
+	ldr r10, [r9]
+	cmp r10, r8
+	ble inPYRloop	/*	loop while j <= 2*i-1	*/
+
 	ldr r0, addrOfNewline	/* print newline for each row	*/
-	bl printf		/* print newline for each row	*/		
+	bl printf		    /* print newline for each row	*/		
 	ldr r3, addrOfI		/* i++	*/
 	ldr r5, [r3]		/* i++	*/
 	add r5, r5, #1		/* i++	*/
@@ -42,18 +55,66 @@ loopRow:
 	blt loopRow	
     b beforeend
 
+	@ inner pyramid loop
+	inPYRloop:
+		@ if (i == 8) print star
+		ldr r5, addrOfI
+		ldr r6, [r5]
+		cmp r6, #8
+		beq starPRINTER
+		@ if (j == 1) print star
+		ldr r5, addrOfJ
+		ldr r6, [r5]
+		cmp r6, #1
+		beq starPRINTER
+		@ if (j == 2*i-1) print star
+		ldr r5, addrOfJ
+		ldr r6, [r5] 
+		ldr r7, addrOfI
+		ldr r8, [r7]
+		@ mul r8, r8, #2
+		@ add r8, #0, r8, LSL #1	/* 	2*i		*/
+		mov r8, r8, LSL #1	/* 	2*i		*/
+		sub r8, r8, #1
+		cmp r6, r8
+		beq starPRINTER
+		@ else print blank
+		push {lr}
+		ldr r0, addrOfBlank
+		bl printf
+		pop {lr}
+		endINpyramid:
+		b end
+
+			@ print stars from conditions of inner loop
+			starPRINTER:
+				push {lr}
+				ldr r0, addrOfStar
+				bl printf
+				pop {lr}
+				b endINpyramid 	/*	exit the inner loop, without printing spaces	*/
+
 loopLblank:
-    push {lr}
-	ldr r0, addrOfBlank	/* load blank char	*/
-	bl printf		/* print blank		*/
-    pop {lr}
-	ldr r3, addrOfJ		/* j++	*/
-	ldr r5, [r3]		/* j++	*/
-	add r5, r5, #1		/* j++	*/
-	str r5, [r3]
-	cmp r5, #8		/* (j < 8)	*/
-	blt loopLblank
-    b end
+        push {lr}
+        ldr r0, addrOfBlank     /* load blank char      */
+        bl printf               /* print blank          */
+        pop {lr}
+
+        ldr r3, addrOfJ         /* j++  */
+        ldr r5, [r3]            /* j++  */
+        add r5, r5, #1          /* j++  */
+        str r5, [r3]
+        cmp r5, #8              /* (j < 8)      */
+        blt loopLblank
+
+        @ printPYRAMID:
+        @         push {lr}
+        @         ldr r0, addrOfStar
+        @         bl printf
+        @         pop {lr}
+
+        b end
+
 
 beforeend:
     ldr r1, addrOfReturn
